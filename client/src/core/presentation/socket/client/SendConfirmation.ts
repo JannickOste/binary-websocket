69a1a0cd8ada10b782c9ef4c1f8eac24bab8299c
Jannick Oste <oste.jannick@hotmail.com>
2025-05-13 17:42:59 +0200

    SendConfirmation packet added to client

---
+++ client/src/core/presentation/socket/client/SendConfirmation.ts
@@ -0,0 +1,33 @@
+import { bindingScopeValues, inject } from "inversify";
+import EncryptionType from "../../../domain/crypt/EncryptionType";
+import Client from "../../../domain/socket/Client";
+import ClientPacket from "../../../domain/socket/ClientPacket";
+import IncommingPacketProcessorInterface from "../../../domain/socket/IncommingPacketProcessorInterface";
+import OutgoingPacketBuilderInterface from "../../../domain/socket/OutgoingPacketBuilderInterface";
+import SocketPacket from "../../../domain/socket/SocketPacket";
+import BufferReader from "../../../domain/utils/BufferReader";
+import types from "../../../../di";
+import RSAInterface from "../../../domain/crypt/RSAInterface";
+import { provide } from "../../../domain/decorators/provide";
+import AESInterface from "../../../domain/crypt/AESInterface";
+
+@provide(types.Core.Infrastructure.Socket.OutgoingPacketBuilderInterface, bindingScopeValues.Singleton)
+export default class SendConfirmation extends SocketPacket implements OutgoingPacketBuilderInterface {
+    id: number = ClientPacket.SendConfirmation;
+
+    constructor(
+    )
+    {
+        super(
+            ClientPacket.SendConfirmation,
+            EncryptionType.AES256CBC
+        );
+    }
+
+    async build(code: Buffer): Promise<SocketPacket> 
+    {
+        this.writeBuffer(code);
+
+        return this;
+    }
+}
---
+++ client/src/core/presentation/socket/server/SendConfirmation.ts
@@ -0,0 +1,25 @@
+import { bindingScopeValues, inject, LazyServiceIdentifier } from "inversify";
+import OutgoingPacketManagerInterface from "../../../domain/socket/manager/OutgoingPacketManagerInterface";
+import IncommingPacketProcessorInterface from "../../../domain/socket/IncommingPacketProcessorInterface";
+import RSAInterface from "../../../domain/crypt/RSAInterface";
+import { provide } from "../../../domain/decorators/provide";
+import ClientPacket from "../../../domain/socket/ClientPacket";
+import BufferReader from "../../../domain/utils/BufferReader";
+import types from "../../../../di";
+import OutgoingPacketManager from "../../../infrastructure/socket/manager/OutgoingPacketManager";
+import AESInterface from "../../../domain/crypt/AESInterface";
+import ServerPacket from "../../../domain/socket/ServerPacket";
+
+@provide(types.Core.Infrastructure.Socket.IncommingPacketProcessorInterface, bindingScopeValues.Singleton)
+export default class SendConfirmation implements IncommingPacketProcessorInterface {
+    id = ServerPacket.SendConfirmation;
+
+    async process(packetReader: BufferReader): Promise<void> {
+        const confirmationCode = packetReader.readBuffer();
+
+        return await OutgoingPacketManager.Singleton.dispatchToServer(
+            ClientPacket.SendConfirmation,
+            confirmationCode
+        )
+    }
+}
