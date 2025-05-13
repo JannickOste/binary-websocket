e28b069ff02000887db0e875beabf3ce5d7d4139
Jannick Oste <oste.jannick@hotmail.com>
2025-05-13 17:57:06 +0200

    wrong AES key usage fix

--- client/src/core/presentation/socket/server/SendAESKey.ts
+++ client/src/core/presentation/socket/server/SendAESKey.ts
@@ -5,24 +5,23 @@ import RSAInterface from "../../../domain/crypt/RSAInterface";
 import { provide } from "../../../domain/decorators/provide";
 import ClientPacket from "../../../domain/socket/ClientPacket";
 import BufferReader from "../../../domain/utils/BufferReader";
-import types from "../../../../di";
+import types, { container } from "../../../../di";
 import OutgoingPacketManager from "../../../infrastructure/socket/manager/OutgoingPacketManager";
 import AESInterface from "../../../domain/crypt/AESInterface";
+import ServerPacket from "../../../domain/socket/ServerPacket";
 
 @provide(types.Core.Infrastructure.Socket.IncommingPacketProcessorInterface, bindingScopeValues.Singleton)
 export default class SendAESKey implements IncommingPacketProcessorInterface {
-    id = ClientPacket.SendAESKey;
+    id = ServerPacket.SendAESKey;
     constructor(
-        @inject(types.Core.Infrastructure.Crypt.IRSAInterface)
+        @inject(types.Core.Infrastructure.Crypt.IAESInterface)
         private readonly aes: AESInterface,
     ) {
     }
 
     async process(packetReader: BufferReader): Promise<void> {
-        console.dir(packetReader.buffer.byteLength)
-
         this.aes.serverKey = packetReader.readBuffer();
         
-        return await OutgoingPacketManager.Singleton.dispatchToServer(ClientPacket.SendAESKey)
+        await OutgoingPacketManager.Singleton.dispatchToServer(ClientPacket.SendAESKey)
     }
 }
