ef33d4bd99ee1ab7c67c8b2c3e23d707ec7c7db6
Jannick Oste <oste.jannick@hotmail.com>
2025-05-16 00:00:28 +0200

    temp static header size added client sided

--- client/src/core/domain/socket/SocketPacket.ts
+++ client/src/core/domain/socket/SocketPacket.ts
@@ -6,6 +6,7 @@ export default class SocketPacket extends BufferWriter
     public static readonly MAX_PACKET_SIZE = 4096;
     public static readonly MAX_PAYLOAD_SIZE = SocketPacket.MAX_PACKET_SIZE;
     public readonly headerSize: number = -1
+    public static HEADER_SIZE = 12;
 
     constructor(
         public readonly id: number,
@@ -14,11 +15,11 @@ export default class SocketPacket extends BufferWriter
     ) {
         super(Buffer.alloc(SocketPacket.MAX_PACKET_SIZE));
 
-        this.writeInt(0)
+        this.writeInt(SocketPacket.HEADER_SIZE+encryption.length) // Temp header size not being properly written
         this.writeInt(id);
         this.writeString(encryption);
 
         this.headerSize = this.currentOffset;
-        this.buffer.writeInt32LE(this.headerSize, 0)
+        this.buffer.writeInt32LE(this.headerSize)
     }
 }
