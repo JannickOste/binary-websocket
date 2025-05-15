5168f8c85b49fe6d67683fa0d130a560d25a43d3
Jannick Oste <oste.jannick@hotmail.com>
2025-05-15 21:45:07 +0200

    additional tests added for bufferWritter

--- server/tests/core/domain/utils/BufferWriter.test.ts
+++ server/tests/core/domain/utils/BufferWriter.test.ts
@@ -1,296 +1,254 @@
 import BufferWriter from "../../../../src/core/domain/utils/BufferWriter";
 
-class TestBufferWriter extends BufferWriter {
-    public writeByte(value: number): void { super.writeByte(value); }
-    public writeShort(value: number, littleEndian: boolean = true): void { super.writeShort(value, littleEndian); }
-    public writeInt(value: number, littleEndian: boolean = true): void { super.writeInt(value, littleEndian); }
-    public writeFloat32(value: number, littleEndian: boolean = true): void { super.writeFloat32(value, littleEndian); }
-    public writeFloat64(value: number, littleEndian: boolean = true): void { super.writeFloat64(value, littleEndian); }
-    public writeBoolean(value: boolean): void { super.writeBoolean(value); }
-    public writeBuffer(value: Buffer): void { super.writeBuffer(value); }
-    public writeString(value: string): void { super.writeString(value); }
-    public assertBufferSize(value: number): void { super.assertBufferSize(value); }
-}
+describe('BufferWriter', () => {
+    let buffer: Buffer;
+    let writer: BufferWriter;
 
-describe("BufferWriter", () => {
+    beforeEach(() => {
+        buffer = Buffer.alloc(64);
+        writer = new BufferWriter(buffer);
+    });
+
     describe("constructor", () => {
-        test("Initializes buffer correctly", () => {
-            const buffer = Buffer.from([0x01, 0x02, 0x03]);
-            const writer = new TestBufferWriter(buffer);
-            expect(writer.buffer.equals(buffer)).toBe(true);
+        it('should initialize with the correct buffer', () => {
+            expect(writer.buffer).toBe(buffer);
+        });
+    })
+
+    describe('writeByte', () => {
+        it('should write a byte to the buffer', () => {
+            writer.writeByte(127);
+            expect(buffer[0]).toBe(127);
+
         });
 
-        test("Initializes currentOffset correctly", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-            expect(writer.currentOffset).toBe(0);
-        });
-    });
+        it("should increment the offset", () => {
+            writer.writeByte(127);
 
-    describe("assertBufferSize", () => {
-        test("Does not throw if buffer size is sufficient", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            expect(() => writer.assertBufferSize(4)).not.toThrow();
-        });
-
-        test("Throws if buffer size is insufficient", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            expect(() => writer.assertBufferSize(5)).toThrow("BufferOverflow: Not enough space in buffer to write data");
-        });
-    });
-
-    describe("writeByte", () => {
-        test("Writes correct value", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeByte(0x80);
-            expect(buffer[0]).toBe(0x80);
-        });
-
-        test("Increments current offset after writing a byte", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-            writer.writeByte(0x80);
             expect(writer.currentOffset).toBe(1);
+        })
+
+        it("should throw an error if the value is outside of range", () => {
+            expect(() => writer.writeByte(128)).toThrow();
+        })
+
+        it('should throw an error when buffer overflows', () => {
+            const writer = new BufferWriter(Buffer.from([]));
+
+            expect(() => writer.writeByte(256)).toThrow();
         });
     });
 
-    describe("writeShort", () => {
-        test("Writes correct little endian value", () => {
-            const buffer = Buffer.alloc(2);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeShort(-32768, true);
-            expect(buffer.readInt16LE(0)).toBe(-32768);
+    describe('writeUByte', () => {
+        it('should write an unsigned byte to the buffer', () => {
+            writer.writeUByte(255);
+            expect(buffer[0]).toBe(255);
         });
 
-        test("Writes correct big endian value", () => {
-            const buffer = Buffer.alloc(2);
-            const writer = new TestBufferWriter(buffer);
+        it("should increment the offset", () => {
+            writer.writeUByte(127);
 
-            writer.writeShort(-32768, false);
-            expect(buffer.readInt16BE(0)).toBe(-32768);
+            expect(writer.currentOffset).toBe(1);
+        })
+
+        it('should throw an error when buffer overflows', () => {
+            const writer = new BufferWriter(Buffer.from([]))
+            expect(() => writer.writeUByte(256)).toThrow()
+        });
+    });
+
+    describe('writeBoolean', () => {
+        it('should write true as 1', () => {
+            writer.writeBoolean(true);
+            expect(buffer[0]).toBe(1);
+
         });
 
-        test("Adds 2 to current offset after writing a short", () => {
-            const buffer = Buffer.alloc(2);
-            const writer = new TestBufferWriter(buffer);
-            writer.writeShort(-32768, true);
+        it('should write false as 0', () => {
+            writer.writeBoolean(false);
+            expect(buffer[1]).toBe(0);
+        });
+    });
+
+    describe('writeShort', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeShort(1234, true);
+
+            expect(buffer.readInt16LE(0)).toBe(1234);
+        });
+
+        it('should write the correct big endian value', () => {
+            writer.writeShort(1234, false);
+
+            expect(buffer.readInt16BE(0)).toBe(1234);
+        });
+
+        it('should make an addition of 2 to the offset', () => {
+            writer.writeShort(1234);
+
             expect(writer.currentOffset).toBe(2);
         });
+
+        it('should throw an error if the value is outside of range', () => {
+            expect(() => writer.writeShort(-32769)).toThrow()
+            expect(() => writer.writeShort(32768)).toThrow()
+        });
     });
 
-    describe("writeInt", () => {
-        test("Writes correct little endian value", () => {
+    describe('writeUShort', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeUShort(1234, true);
 
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeInt(-2147483648, true);
-
-            expect(buffer.readInt32LE(0)).toBe(-2147483648);
+            expect(buffer.readUInt16LE(0)).toBe(1234);
         });
 
-        test("Writes correct big endian value", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
+        it('should write the correct big endian value', () => {
+            writer.writeUShort(1234, false);
 
-            writer.writeInt(-2147483648, false);
-
-            expect(buffer.readInt32BE(0)).toBe(-2147483648);
+            expect(buffer.readUInt16BE(0)).toBe(1234);
         });
 
-        test("Adds 4 to current offset after writing an int", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
+        it('should make an addition of 2 to the offset', () => {
+            writer.writeUShort(1234);
 
-            writer.writeInt(-2147483648, true);
+            expect(writer.currentOffset).toBe(2);
+        });
+
+        it('should throw an error if the value is outside of range', () => {
+            expect(() => writer.writeUShort(-1)).toThrow()
+            expect(() => writer.writeUShort(65536)).toThrow()
+        });
+    });
+
+    describe('writeInt', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeInt(1234, true);
+
+            expect(buffer.readInt32LE(0)).toBe(1234);
+        });
+
+        it('should write the correct big endian value', () => {
+            writer.writeInt(1234, false);
+
+            expect(buffer.readInt32BE(0)).toBe(1234);
+        });
+
+        it('should make an addition of 4 to the offset', () => {
+            writer.writeInt(1234);
+
+            expect(writer.currentOffset).toBe(4);
+        });
+
+        it('should throw an error if the value is outside of range', () => {
+            expect(() => writer.writeInt(-2147483649)).toThrow()
+            expect(() => writer.writeInt(2147483648)).toThrow()
+        });
+    });
+
+    describe('writeUInt', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeUInt(1234, true);
+
+            expect(buffer.readUint32LE(0)).toBe(1234);
+        });
+
+        it('should write the correct big endian value', () => {
+            writer.writeUInt(1234, false);
+
+            expect(buffer.readUint32BE(0)).toBe(1234);
+        });
+
+        it('should make an addition of 4 to the offset', () => {
+            writer.writeUInt(1234);
+
+            expect(writer.currentOffset).toBe(4);
+        });
+
+        it('should throw an error if the value is outside of range', () => {
+            expect(() => writer.writeUInt(-1)).toThrow()
+            expect(() => writer.writeUInt(4294967296)).toThrow()
+        });
+    });
+
+    describe('writeFloat', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeFloat(1234, true);
+
+            expect(buffer.readFloatLE(0)).toBeCloseTo(1234);
+        });
+
+        it('should write the correct big endian value', () => {
+            writer.writeFloat(1234, false);
+
+            expect(buffer.readFloatBE(0)).toBeCloseTo(1234);
+        });
+
+        it('should make an addition of 4 to the offset', () => {
+            writer.writeFloat(1234);
 
             expect(writer.currentOffset).toBe(4);
         });
     });
 
-    describe("writeFloat32", () => {
-        test("Writes correct little endian value", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat32(-2147483648, true);
-            expect(buffer.readFloatLE(0)).toBe(-2147483648);
+    describe('writeDouble', () => {
+        it('should write the correct little endian value', () => {
+            writer.writeDouble(1234.56789, true);
+            expect(buffer.readDoubleLE(0)).toBeCloseTo(1234.56789);
         });
-
-        test("Writes correct big endian value", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat32(-2147483648, false);
-            expect(buffer.readFloatBE(0)).toBe(-2147483648);
+    
+        it('should write the correct big endian value', () => {
+            writer.writeDouble(1234.56789, false);
+            expect(buffer.readDoubleBE(0)).toBeCloseTo(1234.56789);
         });
-
-        test("Adds 4 to current offset after writing a float32", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat32(-2147483648, true);
-            expect(writer.currentOffset).toBe(4);
-        });
-    });
-
-    describe("writeFloat64", () => {
-        test("Writes correct little endian value", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat64(-2147483648, true);
-            expect(buffer.readDoubleLE(0)).toBe(-2147483648);
-        });
-
-        test("Writes correct big endian value", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat64(-2147483648, false);
-            expect(buffer.readDoubleBE(0)).toBe(-2147483648);
-        });
-
-        test("Adds 8 to current offset after writing a float64", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.writeFloat64(-2147483648, true);
+    
+        it('should make an addition of 8 to the offset', () => {
+            writer.writeDouble(1234.56789);
             expect(writer.currentOffset).toBe(8);
         });
     });
 
-    describe("writeBoolean", () => {
-        test("Writes correct value for true", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-    
-            writer.writeBoolean(true);
-            expect(buffer[0]).toBe(1);
-        });
-    
-        test("Writes correct value for false", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-    
-            writer.writeBoolean(false);
-            expect(buffer[0]).toBe(0);
-        });
-    
-        test("Increments current offset after writing a boolean", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-    
-            writer.writeBoolean(true);
-            expect(writer.currentOffset).toBe(1);
-        });
-    });
-    
-    describe("writeBuffer", () => {
-        test("Writes correct value", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            const data = Buffer.from([0x01, 0x02, 0x03, 0x04]);
-            const expected = Buffer.concat([Buffer.from([0x04, 0x00, 0x00, 0x00]), data]);
-            writer.writeBuffer(data);
-
-            expect(buffer.equals(expected)).toBe(true);
-        });
-
-        test("Increments current offset after writing a buffer", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            const data = Buffer.from([0x01, 0x02, 0x03, 0x04]);
-            writer.writeBuffer(data);
-
+    describe('writeDate', () => {
+        it('should write a date to the buffer as a double', () => {
+            const date = new Date('2025-01-01T00:00:00Z');
+            writer.writeDate(date, true);
+            expect(buffer.readDoubleLE(0)).toBe(date.getTime());
             expect(writer.currentOffset).toBe(8);
         });
     });
 
-    describe("writeString", () => {
-        test("Writes correct value", () => {
-            const buffer = Buffer.alloc(9);
-            const writer = new TestBufferWriter(buffer);
-            const data = "Hello";
-
-            const expected = Buffer.concat([Buffer.from([0x05, 0x00, 0x00, 0x00]), Buffer.from(data)]);
-            writer.writeString(data);
-
-            expect(buffer.equals(expected)).toBe(true);
+    describe('writeBuffer', () => {
+        it('should write the length of the buffer first as an int', () => {
+            const smallBuffer = Buffer.from([1, 2, 3, 4]);
+            writer.writeBuffer(smallBuffer);
+    
+            expect(buffer.readInt32LE(0)).toBe(smallBuffer.length);
         });
-        test("Increments current offset after writing a string", () => {
-            const buffer = Buffer.alloc(9);
-            const writer = new TestBufferWriter(buffer);
-            const data = "Hello";
+    
+        it('should write the data after the length of the buffer', () => {
+            const smallBuffer = Buffer.from([1, 2, 3, 4]);
+            writer.writeBuffer(smallBuffer);
+    
+            expect(buffer.subarray(4, 8).toString('hex')).toBe(smallBuffer.toString('hex'));
+        });
+    
+        it('should throw an error if the buffer exceeds available space', () => {
+            const writer = new BufferWriter(Buffer.alloc(4));
+            const smallBuffer = Buffer.from([1, 2, 3, 4]);
+    
+            expect(() => writer.writeBuffer(smallBuffer)).toThrow();
+        });
+    });
 
-            writer.writeString(data);
-
+    describe('writeString', () => {
+        it('should write a string to the buffer', () => {
+            writer.writeString('hello');
+            expect(buffer.subarray(4, 9).toString('utf-8')).toBe('hello');
             expect(writer.currentOffset).toBe(9);
         });
-    });
 
-    describe("write", () => {
-        test("write(Number) writes correct value", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            writer.write(-2147483648);
-            expect(buffer.readFloatLE(0)).toBe(-2147483648);
-        });
-
-        test("write(Boolean) writes correct value", () => {
-            const buffer = Buffer.alloc(1);
-            const writer = new TestBufferWriter(buffer);
-            writer.write(true);
-            expect(buffer[0]).toBe(1);
-        });
-
-        test("write(Date) writes correct value", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-            const date = new Date(0);
-            const expected = Buffer.concat([Buffer.from([0x00, 0x00, 0x00, 0x00]), Buffer.from([0x00, 0x00, 0x00, 0x00])]);
-            writer.write(date);
-
-            expect(buffer.equals(expected)).toBe(true);
-        });
-
-        test("write(String) writes correct value", () => {
-            const buffer = Buffer.alloc(9);
-            const writer = new TestBufferWriter(buffer);
-            const data = "Hello";
-
-            const expected = Buffer.concat([Buffer.from([0x05, 0x00, 0x00, 0x00]), Buffer.from(data)]);
-            writer.write(data);
-
-            expect(buffer.equals(expected)).toBe(true);
-        });
-
-        test("write(Buffer) writes correct value", () => {
-            const buffer = Buffer.alloc(8);
-            const writer = new TestBufferWriter(buffer);
-
-            const data = Buffer.from([0x01, 0x02, 0x03, 0x04]);
-            const expected = Buffer.concat([Buffer.from([0x04, 0x00, 0x00, 0x00]), data]);
-            writer.write(data);
-
-            expect(buffer.equals(expected)).toBe(true);
-        });
-
-        test("write throws error for unsupported type", () => {
-            const buffer = Buffer.alloc(4);
-            const writer = new TestBufferWriter(buffer);
-
-            expect(() => writer.write({} as any)).toThrow("Unsupported type");
+        it('should support different encodings', () => {
+            writer.writeString('hello', 'latin1');
+            expect(buffer.subarray(4, 9).toString('latin1')).toBe('hello');
         });
     });
 });
