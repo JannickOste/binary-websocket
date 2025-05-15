0a076c6f5424453f25d36d440dba1ea30153852a
Jannick Oste <oste.jannick@hotmail.com>
2025-05-15 20:02:27 +0200

    rsaType removed from constructor and moved to function for flexibility

--- server/src/core/infrastructure/crypt/RSA.ts
+++ server/src/core/infrastructure/crypt/RSA.ts
@@ -4,6 +4,7 @@ import { provide } from "../../domain/decorators/provide";
 import types from "../../../di";
 import { bindingScopeValues } from "inversify";
 import { unmanaged } from "inversify";
+import EncryptionType from "../../domain/crypt/EncryptionType";
 
 @provide(types.Core.Infrastructure.Crypt.IRSAInterface, bindingScopeValues.Singleton)
 export default class RSA implements RSAInterface {
@@ -15,16 +16,12 @@ export default class RSA implements RSAInterface {
         return forge.pki.publicKeyToRSAPublicKeyPem(this.keyPair.publicKey);
     }
 
-    constructor(
-        @unmanaged() private readonly rsaType: forge.pki.rsa.EncryptionScheme = "RSA-OAEP"
-    ) {}
-
-    encrypt(data: Buffer, publicKeyPem: string): Buffer {
+    encrypt(data: Buffer, publicKeyPem: string, rsaType = EncryptionType.RSAOAEP): Buffer {
         const buffer = forge.util.createBuffer(data);
         let encrypted: forge.Bytes | undefined;
 
-        switch(this.rsaType) {
-            case "NONE":
+        switch(rsaType) {
+            case "none":
                 encrypted = buffer.bytes();
                 break;
 
@@ -32,7 +29,7 @@ export default class RSA implements RSAInterface {
                 case "RSAES-PKCS1-V1_5":
                     const publicKeyToUse = forge.pki.publicKeyFromPem(publicKeyPem)
     
-                    encrypted = publicKeyToUse.encrypt(buffer.bytes(), this.rsaType);
+                    encrypted = publicKeyToUse.encrypt(buffer.bytes(), rsaType);
                     break;
 
             default:
@@ -42,17 +39,17 @@ export default class RSA implements RSAInterface {
         return Buffer.from(encrypted, 'binary');
     }
 
-    decrypt(encryptedData: Buffer): Buffer {
+    decrypt(encryptedData: Buffer, rsaType: EncryptionType = EncryptionType.RSAOAEP): Buffer {
         const buffer = forge.util.createBuffer(encryptedData.toString('binary'));
         let decrypted: forge.Bytes | undefined;
 
-        switch(this.rsaType) {
-            case "NONE":
+        switch(rsaType) {
+            case "none":
                 return encryptedData;
 
             case "RSA-OAEP":
             case "RSAES-PKCS1-V1_5":
-                decrypted = this.keyPair.privateKey.decrypt(buffer.bytes(), this.rsaType);
+                decrypted = this.keyPair.privateKey.decrypt(buffer.bytes(), rsaType);
                 break;
 
             default:
