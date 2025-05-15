853b3ae0365fb612cd8d36d2feebe374cd21c0b6
Jannick Oste <oste.jannick@hotmail.com>
2025-05-15 20:07:09 +0200

    test adjusted for rsa encryption mode change

--- server/tests/core/infrastructure/crypt/RSA.test.ts
+++ server/tests/core/infrastructure/crypt/RSA.test.ts
@@ -1,51 +1,63 @@
-import { pki } from "node-forge";
+import EncryptionType from "../../../../src/core/domain/crypt/EncryptionType";
 import RSA from "../../../../src/core/infrastructure/crypt/RSA";
 
 describe("RSA", () => {
     const testData = Buffer.from("Test data");
+    let rsaService: RSA;
+    let publicKey: string;
 
     beforeEach(() => {
-        jest.resetModules(); 
+        rsaService = new RSA();
+        publicKey = rsaService.publicKey;
     });
 
-    describe("encrypt/decrypt", () => {
-        it("should correctly handle the 'NONE' rsaType", () => {
-            const rsaService = new RSA("NONE");  
-
-            const encryptedData = rsaService.encrypt(testData, rsaService.publicKey);
-            const decryptedData = rsaService.decrypt(encryptedData);
-
-            expect(decryptedData.toString("hex")).toEqual(testData.toString("hex"));
+    describe("encrypt", () => {
+        it("should return raw data for 'none' rsaType", () => {
+            const encrypted = rsaService.encrypt(testData, publicKey, EncryptionType.NONE);
+            expect(encrypted.toString("hex")).toEqual(testData.toString("hex"));
         });
 
-        it("should correctly handle the 'RSA-OAEP' rsaType", () => {
-            const rsaService = new RSA("RSA-OAEP");  
-
-            const encryptedData = rsaService.encrypt(testData, rsaService.publicKey);
-            const decryptedData = rsaService.decrypt(encryptedData);
-
-            expect(decryptedData.toString("hex")).toEqual(testData.toString("hex"));
+        it("should encrypt with 'RSA-OAEP'", () => {
+            const encrypted = rsaService.encrypt(testData, publicKey, EncryptionType.RSAOAEP);
+            expect(Buffer.isBuffer(encrypted)).toBe(true);
+            expect(encrypted.equals(testData)).toBe(false); // should not be plaintext
         });
 
-        it("should correctly handle the 'RSAES-PKCS1-V1_5' rsaType", () => {
-            const rsaService = new RSA("RSAES-PKCS1-V1_5");  
-
-            const encryptedData = rsaService.encrypt(testData, rsaService.publicKey);
-            const decryptedData = rsaService.decrypt(encryptedData);
-
-            expect(decryptedData.toString("hex")).toEqual(testData.toString("hex"));
+        it("should encrypt with 'RSAES-PKCS1-V1_5'", () => {
+            const encrypted = rsaService.encrypt(testData, publicKey, EncryptionType.RSAESPKCS);
+            expect(Buffer.isBuffer(encrypted)).toBe(true);
+            expect(encrypted.equals(testData)).toBe(false);
         });
 
-        it("should throw an error on unsupported encryption types", () => {
-            const rsaService = new RSA("UNSUPPORTED" as pki.rsa.EncryptionScheme);  
+        it("should throw error on unsupported encryption type", () => {
+            expect(() =>
+                rsaService.encrypt(testData, publicKey, EncryptionType.AES256CBC)
+            ).toThrow("Unknown RSA Encryption type");
+        });
+    });
 
-            expect(() => rsaService.encrypt(testData, rsaService.publicKey)).toThrow();
+    describe("decrypt", () => {
+        it("should return raw data for 'none' rsaType", () => {
+            const decrypted = rsaService.decrypt(testData, EncryptionType.NONE);
+            expect(decrypted.toString("hex")).toEqual(testData.toString("hex"));
         });
 
-        it("should throw an error on unsupported decryption types", () => {
-            const rsaService = new RSA("UNSUPPORTED" as pki.rsa.EncryptionScheme);  
+        it("should decrypt 'RSA-OAEP' correctly", () => {
+            const encrypted = rsaService.encrypt(testData, publicKey, EncryptionType.RSAOAEP);
+            const decrypted = rsaService.decrypt(encrypted, EncryptionType.RSAOAEP);
+            expect(decrypted.toString("hex")).toEqual(testData.toString("hex"));
+        });
 
-            expect(() => rsaService.decrypt(testData)).toThrow();
+        it("should decrypt 'RSAES-PKCS1-V1_5' correctly", () => {
+            const encrypted = rsaService.encrypt(testData, publicKey, EncryptionType.RSAESPKCS);
+            const decrypted = rsaService.decrypt(encrypted, EncryptionType.RSAESPKCS);
+            expect(decrypted.toString("hex")).toEqual(testData.toString("hex"));
+        });
+
+        it("should throw error on unsupported decryption type", () => {
+            expect(() =>
+                rsaService.decrypt(testData, "INVALID_TYPE" as any)
+            ).toThrow("Unknown RSA Decryption type");
         });
     });
 });
